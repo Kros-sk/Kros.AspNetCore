c81fd3da850ff0b254b9aadd676ccc96e29eaa55
Stano Peťko <stano@stanopetko.eu>
2019-04-17 22:44:14 +0200

    Some styling.

--- src/Kros.AspNetCore/Extensions/HttpResponseExtensions.cs
+++ src/Kros.AspNetCore/Extensions/HttpResponseExtensions.cs
@@ -11,9 +11,11 @@ namespace Kros.AspNetCore.Extensions
     /// </summary>
     public static class HttpResponseExtensions
     {
-        private static readonly HashSet<string> _preservedHeaders = new HashSet<string>{
+        private static readonly HashSet<string> _preservedHeaders = new HashSet<string>
+        {
             HeaderNames.AccessControlAllowCredentials, HeaderNames.AccessControlAllowHeaders,
-            HeaderNames.AccessControlAllowOrigin, HeaderNames.AccessControlAllowMethods };
+            HeaderNames.AccessControlAllowOrigin, HeaderNames.AccessControlAllowMethods
+        };
 
         /// <summary>
         /// Clears <paramref name="response"/> and preserves CORS headers.
--- src/Kros.AspNetCore/Helpers.cs
+++ src/Kros.AspNetCore/Helpers.cs
@@ -7,7 +7,8 @@
     {
         /// <summary>
         /// Get section name by <typeparamref name="TOptions"/> type.
-        /// Section name is class name without Options suffix.
+        /// Section name is class name without <c>Options</c> suffix, so if class name is <c>SmtpOptions</c>, the section
+        /// name is <c>Smtp</c>.
         /// </summary>
         /// <typeparam name="TOptions">Option type.</typeparam>
         /// <returns>Class name without Options suffix.</returns>
