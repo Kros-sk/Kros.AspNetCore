e8871942722ff754a352594a6a088d97c7db1ca2
Lukáš Šefčík <lukas.sefcik@gmail.com>
2019-04-26 12:29:38 +0200

    Added base Startup class.

---
+++ src/Kros.AspNetCore/BaseStartup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Kros.AspNetCore
+{
+    /// <summary>
+    /// Base class for Startup classes in services.
+    /// </summary>
+    public abstract class BaseStartup
+    {
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="env">Information about the web hosting environment.</param>
+        public BaseStartup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile($"appsettings.local.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        /// <summary>
+        /// Application configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+    }
+}
