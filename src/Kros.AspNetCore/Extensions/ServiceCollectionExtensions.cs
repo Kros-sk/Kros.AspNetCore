df301d64412b60ed43641990000dddb8314eca73
Stano Peťko <stano@stanopetko.eu>
2019-05-21 14:01:36 +0200

    Explicit compatilibity version for MVC (#17)

--- src/Kros.AspNetCore/Extensions/ServiceCollectionExtensions.cs
+++ src/Kros.AspNetCore/Extensions/ServiceCollectionExtensions.cs
@@ -10,6 +10,11 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class ServiceCollectionExtensions
     {
         /// <summary>
+        /// Default MVC compatibility version for <see cref="AddWebApi(IServiceCollection)"/>.
+        /// </summary>
+        public static CompatibilityVersion DefaultCompatibilityVersion => CompatibilityVersion.Version_2_2;
+
+        /// <summary>
         /// Configure options of type <typeparamref name="TOptions"/> and binds it to section with the name
         /// same as the class <typeparamref name="TOptions"/> without Options suffix in <paramref name="configuration"/>.
         /// </summary>
@@ -41,11 +46,22 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Adds the minimum essential MVC services to the DI container for web API services.
         /// (MVC Core, JSON Formatters, CORS, API Explorer)
         /// Additional services must be added separately using the <see cref="IMvcCoreBuilder"/> returned from this method.
+        /// As compatibility version for MVC is used <see cref="DefaultCompatibilityVersion"/>.
         /// </summary>
         /// <param name="services">MVC Core builder.</param>
         public static IMvcCoreBuilder AddWebApi(this IServiceCollection services)
+            => AddWebApi(services, DefaultCompatibilityVersion);
+
+        /// <summary>
+        /// Adds the minimum essential MVC services to the DI container for web API services.
+        /// (MVC Core, JSON Formatters, CORS, API Explorer)
+        /// Additional services must be added separately using the <see cref="IMvcCoreBuilder"/> returned from this method.
+        /// </summary>
+        /// <param name="services">MVC Core builder.</param>
+        /// <param name="compatibilityVersion">Compatibility version for MVC.</param>
+        public static IMvcCoreBuilder AddWebApi(this IServiceCollection services, CompatibilityVersion compatibilityVersion)
         {
-            var builder = services.AddMvcCore().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var builder = services.AddMvcCore().SetCompatibilityVersion(compatibilityVersion);
 
             return builder.AddFormatterMappings()
                 .AddJsonFormatters()
