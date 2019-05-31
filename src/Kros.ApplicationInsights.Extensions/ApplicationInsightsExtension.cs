6599654b68b11443b40c6690714e5988effbce71
Lukáš Šefčík <lukas.sefcik@gmail.com>
2019-05-31 12:40:14 +0200

    Added application insights extensions.

---
+++ src/Kros.ApplicationInsights.Extensions/ApplicationInsightsExtension.cs
@@ -0,0 +1,43 @@
+﻿using Kros.ApplicationInsights.Extensions.Options;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kros.ApplicationInsights.Extensions
+{
+    public static partial class ApplicationInsightsExtension
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        private const string ApplicationInsightsSectionName = "ApplicationInsights";
+#pragma warning restore IDE1006 // Naming Styles
+
+        /// <summary>
+        /// Registers application telemetry into DI container.
+        /// </summary>
+        /// <param name="services">IoC container.</param>
+        /// <param name="configuration">Configuration.</param>
+        public static IServiceCollection AddApplicationInsights(this IServiceCollection services, IConfiguration configuration)
+        {
+            ApplicationInsightsOptions options = GetApplicationInsightsOptions(configuration);
+
+            if (options != null)
+            {
+                services.AddSingleton<ITelemetryInitializer>(new CloudRoleNameInitializer(options.ServiceName));
+            }
+
+            return services;
+        }
+
+        private static ApplicationInsightsOptions GetApplicationInsightsOptions(IConfiguration configuration)
+        {
+            IConfigurationSection configurationSection = configuration.GetSection(ApplicationInsightsSectionName);
+
+            if (!configurationSection.Exists())
+            {
+                return null;
+            }
+
+            return configurationSection.Get<ApplicationInsightsOptions>();
+        }
+    }
+}
---
+++ src/Kros.ApplicationInsights.Extensions/Options/ApplicationInsightsOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Kros.ApplicationInsights.Extensions.Options
+{
+    /// <summary>
+    /// Settings for Application insights.
+    /// </summary>
+    public class ApplicationInsightsOptions
+    {
+        /// <summary>
+        /// Service name, which will be display in Application map.
+        /// </summary>
+        public string ServiceName { get; set; }
+    }
+}
