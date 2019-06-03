d367b38624ac9de1b1548d8ae77153dae74b9f3e
Lukáš Šefčík <lukas.sefcik@gmail.com>
2019-06-03 08:20:34 +0200

    Added application insights extensions. (#20)
    * Added application insights extensions.
    
    * Changed namespace name.
    
    * Updated package information.
    
    * Updated build script.

---
+++ src/Kros.ApplicationInsights.Extensions/CloudRoleNameInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Kros.Utils;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Configuration of service name representation in Application Insights.
+    /// </summary>
+    /// <seealso cref="Microsoft.ApplicationInsights.Extensibility.ITelemetryInitializer" />
+    public class CloudRoleNameInitializer : ITelemetryInitializer
+    {
+        private readonly string _serviceName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloudRoleNameInitializer"/> class.
+        /// </summary>
+        /// <param name="serviceName">Service name.</param>
+        public CloudRoleNameInitializer(string serviceName)
+        {
+            _serviceName = Check.NotNullOrWhiteSpace(serviceName, nameof(serviceName));
+        }
+
+        /// <summary>
+        /// Initializes properties of the specified <see cref="T:Microsoft.ApplicationInsights.Channel.ITelemetry" /> object.
+        /// </summary>
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Cloud.RoleName = _serviceName;
+            telemetry.Context.Cloud.RoleInstance = _serviceName;
+        }
+    }
+}
---
+++ src/Kros.ApplicationInsights.Extensions/Options/ApplicationInsightsOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.DependencyInjection.Options
+{
+    /// <summary>
+    /// Settings for Application insights.
+    /// </summary>
+    public class ApplicationInsightsOptions
+    {
+        /// <summary>
+        /// Service name, which will be display in Application map.
+        /// </summary>
+        public string ServiceName { get; set; }
+    }
+}
