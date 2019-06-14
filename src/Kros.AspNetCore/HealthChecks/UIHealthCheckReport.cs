a34c3d252aab04e2b19589b8246df944f8c65597
Marián Mikula <marian.mikula@kros.sk>
2019-06-14 14:20:46 +0200

    JSON output for health checks + UT

---
+++ src/Kros.AspNetCore/HealthChecks/UIHealthCheckReport.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+
+namespace Kros.AspNetCore.HealthChecks
+{
+    /// <summary>
+    /// Models for health checks UI Client. This models represent a indirection between HealthChecks API and
+    /// UI Client in order to implement some features not present on HealthChecks of substiture
+    /// some properties etc.
+    /// </summary>
+    public class UIHealthCheckReport
+    {
+        /// <summary>
+        /// Health check status.
+        /// </summary>
+        public HealthStatus Status { get; set; }
+
+        /// <summary>
+        /// Health check duration.
+        /// </summary>
+        public TimeSpan TotalDuration { get; set; }
+
+        /// <summary>
+        /// Health check entries.
+        /// </summary>
+        public Dictionary<string, HealthCheckReportEntry> Entries { get; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="entries"></param>
+        /// <param name="totalDuration"></param>
+        public UIHealthCheckReport(Dictionary<string, HealthCheckReportEntry> entries, TimeSpan totalDuration)
+        {
+            Entries = entries;
+            TotalDuration = totalDuration;
+        }
+
+        /// <summary>
+        /// Create health check report.
+        /// </summary>
+        /// <param name="report">Report.</param>
+        public static UIHealthCheckReport CreateFrom(HealthReport report)
+        {
+            var uiReport = new UIHealthCheckReport(new Dictionary<string, HealthCheckReportEntry>(), report.TotalDuration)
+            {
+                Status = report.Status,
+            };
+
+            foreach (var item in report.Entries)
+            {
+                var entry = new HealthCheckReportEntry()
+                {
+                    Data = item.Value.Data,
+                    Description = item.Value.Description,
+                    Duration = item.Value.Duration,
+                    Status = item.Value.Status
+                };
+
+                if (item.Value.Exception != null)
+                {
+                    var message = item.Value.Exception?
+                        .Message
+                        .ToString();
+
+                    entry.Exception = message;
+                    entry.Description = item.Value.Description ?? message;
+                }
+
+                uiReport.Entries.Add(item.Key, entry);
+            }
+
+            return uiReport;
+        }
+    }
+
+    /// <summary>
+    /// Info about health check report.
+    /// </summary>
+    public class HealthCheckReportEntry
+    {
+        /// <summary>
+        /// Health check data.
+        /// </summary>
+        public IReadOnlyDictionary<string, object> Data { get; set; }
+
+        /// <summary>
+        /// Health check description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Health check duration.
+        /// </summary>
+        public TimeSpan Duration { get; set; }
+
+        /// <summary>
+        /// Health check exception.
+        /// </summary>
+        public string Exception { get; set; }
+
+        /// <summary>
+        /// Health check status.
+        /// </summary>
+        public HealthStatus Status { get; set; }
+    }
+}
---
+++ tests/Kros.AspNetCore.Tests/HealthChecks/HealthCheckResponseWriterShould.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Kros.AspNetCore.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace Kros.AspNetCore.Tests.HealthChecks
+{
+    public class HealthCheckResponseWriterShould
+    {
+        private HealthReport CreateHealthReport(HealthStatus status)
+        {
+            var entries = new Dictionary<string, HealthReportEntry>();
+            entries.Add("Health_Test_Key", new HealthReportEntry(status, null, new TimeSpan(0, 0, 5), null, null));
+
+            var report = new HealthReport(entries, new TimeSpan(0, 0, 5));
+
+            return report;
+        }
+
+        [Fact]
+        public async void WriteToHttpResponse()
+        {
+            HealthReport report = CreateHealthReport(HealthStatus.Healthy);
+
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            await HealthCheckResponseWriter.WriteHealthCheckResponseAsync(context, report);
+
+            context.Response.Body.Seek(0, SeekOrigin.Begin);
+            string responseBody = new StreamReader(context.Response.Body).ReadToEnd();
+
+            var uiHealthReport = JsonConvert.DeserializeObject<UIHealthCheckReport>(responseBody);
+
+            uiHealthReport.Status.Should().Be(HealthStatus.Healthy);
+            uiHealthReport.TotalDuration.Seconds.Should().Be(5);
+            uiHealthReport.Entries.Count.Should().Be(report.Entries.Count);
+        }
+    }
+}
