e71a9506d5e9b1e9209ad9207c7c96da2487585e
Marián Mikula <marian.mikula@kros.sk>
2019-06-14 14:25:49 +0200

    Fix problem with CodeFactor (more class in one file)

--- src/Kros.AspNetCore/HealthChecks/UIHealthCheckReport.cs
+++ src/Kros.AspNetCore/HealthChecks/UIHealthCheckReport.cs
@@ -74,35 +74,4 @@ namespace Kros.AspNetCore.HealthChecks
             return uiReport;
         }
     }
-
-    /// <summary>
-    /// Info about health check report.
-    /// </summary>
-    public class HealthCheckReportEntry
-    {
-        /// <summary>
-        /// Health check data.
-        /// </summary>
-        public IReadOnlyDictionary<string, object> Data { get; set; }
-
-        /// <summary>
-        /// Health check description.
-        /// </summary>
-        public string Description { get; set; }
-
-        /// <summary>
-        /// Health check duration.
-        /// </summary>
-        public TimeSpan Duration { get; set; }
-
-        /// <summary>
-        /// Health check exception.
-        /// </summary>
-        public string Exception { get; set; }
-
-        /// <summary>
-        /// Health check status.
-        /// </summary>
-        public HealthStatus Status { get; set; }
-    }
 }
