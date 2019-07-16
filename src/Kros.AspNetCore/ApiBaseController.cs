8915071118534db44704ce16765818d6a45568f0
Marián Mikula <marian.mikula@kros.sk>
2019-07-16 10:16:59 +0200

    Add base controller for all api controllers + UT

---
+++ src/Kros.AspNetCore/ApiBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kros.AspNetCore
+{
+    /// <summary>
+    /// A base class for all api controllers.
+    /// </summary>
+    [ApiController]
+    [Authorize]
+    [Route("/api/[controller]")]
+    public abstract class ApiBaseController : ControllerBase
+    {
+    }
+}
