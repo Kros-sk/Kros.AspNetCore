7e4364fcaee264404d84085e13a15667771e95f9
Miňo Martiniak <mino.martiniak@gmail.com>
2019-07-26 12:06:35 +0200

    Patch map (#41)
    * Add basic implementation for mapping JsonPatchDocument to database names.
    
    * Document JsonPatchDocumentExtension
    
    * Update src/Kros.AspNetCore/JsonPatch/IJsonPatchMapper.cs
    
    Co-Authored-By: vojtechrojicek <42407746+vojtechrojicek@users.noreply.github.com>
    
    * Update src/Kros.AspNetCore/JsonPatch/JsonPatchMapperConfig.cs
    
    Co-Authored-By: vojtechrojicek <42407746+vojtechrojicek@users.noreply.github.com>
    
    * Update src/Kros.AspNetCore/README.md
    
    Co-Authored-By: vojtechrojicek <42407746+vojtechrojicek@users.noreply.github.com>
    
    * Fix comment from PR
    
    * Simplify of replacing /

---
+++ src/Kros.AspNetCore/JsonPatch/JsonPatchMapperConfig.cs
@@ -0,0 +1,59 @@
+﻿using Kros.Extensions;
+using System;
+using System.Collections.Concurrent;
+
+namespace Kros.AspNetCore.JsonPatch
+{
+    /// <summary>
+    ///JSON PATCH mapper configuration. Configuration for mapping <see cref="JsonPatchMapperConfig{TSource}"/>
+    ///operation paths to database columns.
+    /// </summary>
+    /// <typeparam name="TSource">Source model type.</typeparam>
+    public class JsonPatchMapperConfig<TSource> : IJsonPatchMapper where TSource : class
+    {
+        private Func<string, string> _pathMapping;
+        private ConcurrentDictionary<string, string> _mapping =
+            new ConcurrentDictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+
+        /// <summary>
+        /// Creates new config for <typeparamref name="TSource"/> and store it for non parametric  extension
+        /// <see cref="JsonPatchDocumentExtensions.GetColumnsNames{TModel}(Microsoft.AspNetCore.JsonPatch.JsonPatchDocument{TModel})"/>.
+        /// </summary>
+        /// <returns>New configuration.</returns>
+        public static JsonPatchMapperConfig<TSource> NewConfig()
+        {
+            var config = new JsonPatchMapperConfig<TSource>();
+
+            JsonPatchMapperConfigStore.Instance.Add(config);
+
+            return config;
+        }
+
+        /// <summary>
+        /// Defines mapping rule for JSON patch operation path to column name.
+        /// </summary>
+        /// <param name="pathMapping">Mapping rule function.</param>
+        /// <returns>Configuration instance for next fluent configuration.</returns>
+        /// <remarks>
+        /// When you don't want a map path, then return <see langword="null"/> from <paramref name="pathMapping"/>.
+        /// </remarks>
+        public JsonPatchMapperConfig<TSource> Map(Func<string, string> pathMapping)
+        {
+            _pathMapping = pathMapping;
+            return this;
+        }
+
+        string IJsonPatchMapper.GetColumnName(string path)
+            => _mapping.GetOrAdd(path, GetColumnNameInternal);
+
+        private string GetColumnNameInternal(string path)
+        {
+            if (_pathMapping != null)
+            {
+                path = _pathMapping(path);
+            }
+
+            return path?.Replace("/", string.Empty);
+        }
+    }
+}
---
+++ src/Kros.AspNetCore/JsonPatch/JsonPatchMapperConfigStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Kros.AspNetCore.JsonPatch
+{
+    /// <summary>
+    /// Internal store for JSON patch mapping configuration.
+    /// </summary>
+    internal class JsonPatchMapperConfigStore
+    {
+        private ConcurrentDictionary<Type, object> _configs = new ConcurrentDictionary<Type, object>();
+        private static JsonPatchMapperConfigStore _instance;
+
+        /// <summary>
+        /// Instance.
+        /// </summary>
+        public static JsonPatchMapperConfigStore Instance => _instance ?? (_instance = new JsonPatchMapperConfigStore());
+
+        private JsonPatchMapperConfigStore()
+        {
+        }
+
+        /// <summary>
+        /// Get configuration for mapping JSON patch of <typeparamref name="TSource"/> model to database names.
+        /// </summary>
+        /// <typeparam name="TSource">Type of model.</typeparam>
+        /// <returns>Configuration for mapping JSON patch of <typeparamref name="TSource"/> model to database names.</returns>
+        public JsonPatchMapperConfig<TSource> GetConfig<TSource>() where TSource : class
+            => _configs.GetOrAdd(typeof(TSource), (t) => new JsonPatchMapperConfig<TSource>()) as JsonPatchMapperConfig<TSource>;
+
+        /// <summary>
+        /// Store configuration for mapping JSON patch of <typeparamref name="TSource"/> model to database names.
+        /// </summary>
+        /// <typeparam name="TSource">Type of model.</typeparam>
+        /// <param name="jsonPatchMapperConfig">
+        /// Configuration for mapping JSON patch of <typeparamref name="TSource"/> model to database names.
+        /// </param>
+        public void Add<TSource>(JsonPatchMapperConfig<TSource> jsonPatchMapperConfig) where TSource: class
+            => _configs.TryAdd(typeof(TSource), jsonPatchMapperConfig);
+    }
+}
