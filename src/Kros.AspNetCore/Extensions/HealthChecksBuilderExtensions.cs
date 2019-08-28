b9d3e35450d0e0395ad0d67cbe56a30e8f63d6a9
kumecky <kumecky@kros.sk>
2019-08-28 14:29:44 +0200

    healthchecks extensions added

---
+++ src/Kros.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Kros.Identity.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Kros.AspNetCore.Extensions
+{
+    public static class HealthChecksBuilderExtensions
+    {
+        public static IHealthChecksBuilder AddIdentityServerHealthChecks(this IHealthChecksBuilder healthChecksBuilder, IConfiguration configuration)
+        {
+            var identityServerOptions = configuration.GetSection("IdentityServerHandlers").Get<IdentityServerOptions[]>().ToList();
+            foreach (var option in identityServerOptions)
+            {
+                healthChecksBuilder.AddUrlGroup(
+                    new Uri(option.AuthorityUrl),
+                    name: $" IdentityServer handler for scheme {option.AuthenticationScheme}",
+                    tags: new string[] { "url" });
+            }
+
+            return healthChecksBuilder;
+        }
+    }
+}
