693dc4787998878835364f8d0e4c427ba9dcd4a6
kumecky <kumecky@kros.sk>
2019-09-02 07:16:34 +0200

    PR changes

--- src/Kros.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
+++ src/Kros.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
@@ -2,20 +2,29 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Linq;
+using System.Collections.Generic;
 
 namespace Kros.AspNetCore.Extensions
 {
+    /// <summary>
+    /// Health checks builder extension.
+    /// </summary>
     public static class HealthChecksBuilderExtensions
     {
+        /// <summary>
+        /// Adds identity server health check.
+        /// </summary>
+        /// <param name="healthChecksBuilder">Health check builder</param>
+        /// <param name="configuration">Configuration</param>
+        /// <returns></returns>
         public static IHealthChecksBuilder AddIdentityServerHealthChecks(this IHealthChecksBuilder healthChecksBuilder, IConfiguration configuration)
         {
-            var identityServerOptions = configuration.GetSection("IdentityServerHandlers").Get<IdentityServerOptions[]>().ToList();
-            foreach (var option in identityServerOptions)
+            IEnumerable<IdentityServerOptions> identityServerOptions = configuration.GetSection<IdentityServerOptions[]>("IdentityServerHandlers");
+            foreach (IdentityServerOptions option in identityServerOptions)
             {
                 healthChecksBuilder.AddUrlGroup(
                     new Uri(option.AuthorityUrl),
-                    name: $" IdentityServer handler for scheme {option.AuthenticationScheme}",
+                    name: $"IdentityServer handler for scheme {option.AuthenticationScheme}",
                     tags: new string[] { "url" });
             }
 
