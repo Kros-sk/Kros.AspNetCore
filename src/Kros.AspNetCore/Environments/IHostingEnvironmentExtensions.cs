ca7b61ae11181a6f7b6c4f71f0d475c32c283d9c
wosciatko <54631482+wosciatko@users.noreply.github.com>
2019-10-02 13:55:08 +0200

    Added "Test" environment support (#50)
    * Added exception handlig GatewayAuthorizationMiddleware
    
    * Little refactor + fixed and added tests for exceptions
    
    * Blank line removed
    
    * Added Test environment and extensions IHostedEnvironment
    
    * Comments changes and little refactor in tests

---
+++ src/Kros.AspNetCore/Environments/IHostingEnvironmentExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Microsoft.AspNetCore.Hosting
+{
+    /// <summary>
+    /// Extension for IHostingEnvironment.
+    /// </summary>
+    public static class IHostingEnvironmentExtensions
+    {
+        /// <summary>
+        /// Check if current environment is <see cref="Environments.Test"/>.
+        /// </summary>
+        /// <param name="env">Current environment.</param>
+        /// <returns><see langword="true" /> if current environment is <see cref="Environments.Test"/>,
+        /// <see langword="false" /> otherwise.</returns>
+        public static bool IsTest(this IHostingEnvironment env)
+            => env.IsEnvironment(Environments.Test);
+
+        /// <summary>
+        /// Check if current environment is <see cref="Environments.Test"/> or <see cref="Environments.Development"/>.
+        /// </summary>
+        /// <param name="env">Current environment.</param>
+        /// <returns><see langword="true" /> if current environment is <see cref="Environments.Test"/> or
+        /// <see cref="Environments.Development"/>, <see langword="false" /> otherwise.</returns>
+        public static bool IsTestOrDevelopment(this IHostingEnvironment env)
+            => env.IsTest() || env.IsDevelopment();
+
+        /// <summary>
+        /// Check if current environment is <see cref="Environments.Staging"/> or <see cref="Environments.Production"/>.
+        /// </summary>
+        /// <param name="env">Current environment.</param>
+        /// <returns><see langword="True" /> if current environment is <see cref="Environments.Staging"/> or
+        /// <see cref="Environments.Production"/>, <see langword="false" /> otherwise.</returns>
+        public static bool IsStagingOrProduction(this IHostingEnvironment env)
+            => env.IsStaging() || env.IsProduction();
+    }
+}
---
+++ tests/Kros.AspNetCore.Tests/Environments/EnvironmentExtensionsShould.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using NSubstitute;
+using Xunit;
+
+namespace Kros.AspNetCore.Tests.Environments
+{
+    public class EnvironmentExtensionsShould
+    {
+        [Fact]
+        public void IsTestReturnTrueIfTestEnvironment()
+        {
+            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Test);
+
+            env.IsTest().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsTestReturnFalseIfDevelopmentEnvironment()
+        {
+            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Development);
+            env.IsTest().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsTestOrDevelopmentReturnTrueIfDevelopmentEnvironment()
+        {
+            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Development);
+            env.IsTestOrDevelopment().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsTestOrDevelopmentReturnFalseIfStagingEnvironment()
+        {
+            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Staging);
+            env.IsTestOrDevelopment().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsStagingOrProductionReturnTrueIfStagingEnvironment()
+        {
+            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Staging);
+            env.IsStagingOrProduction().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsStagingOrProductionReturnFalseIfTestEnvironment()
+        {
+            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Test);
+            env.IsStagingOrProduction().Should().BeFalse();
+        }
+    }
+}
