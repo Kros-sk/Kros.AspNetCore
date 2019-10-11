abe6f96ea286642cb5522a63ac259cc8e6dd7064
Pavel Urban <pavel.urban1@gmail.com>
2019-10-11 13:45:54 +0200

    Catch simplified

--- src/Kros.AspNetCore/Extensions/HttpClientFactoryExtensions.cs
+++ src/Kros.AspNetCore/Extensions/HttpClientFactoryExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -26,7 +25,7 @@ namespace Kros.AspNetCore.Net
                     client.DefaultRequestHeaders.Authorization = newVal;
                 }
             }
-            catch (Exception)
+            catch
             {
                 client.Dispose();
                 throw;
