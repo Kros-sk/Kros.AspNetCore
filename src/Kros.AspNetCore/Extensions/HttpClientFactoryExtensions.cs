97a6523cb215d8c25f63672c887c0f9a1dfca074
Pavel Urban <pavel.urban1@gmail.com>
2019-10-11 13:06:40 +0200

    HttpClientFactoryExtension and unit test

---
+++ src/Kros.AspNetCore/Extensions/HttpClientFactoryExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Kros.AspNetCore.Net
+{
+    /// <summary>
+    /// Extensions for HttpClientFactory.
+    /// </summary>
+    public static class HttpClientFactoryExtensions
+    {
+        /// <summary>
+        /// Creates HttpClient with authorization header from <paramref name="httpContextAccessor"/>.
+        /// </summary>
+        public static HttpClient CreateClientWithAuthorization(this IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            HttpClient client = httpClientFactory.CreateClient();
+            try
+            {
+                string authHeader = httpContextAccessor.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+                if (authHeader != null)
+                {
+                    AuthenticationHeaderValue.TryParse(authHeader, out AuthenticationHeaderValue newVal);
+                    client.DefaultRequestHeaders.Authorization = newVal;
+                }
+            }
+            catch (Exception)
+            {
+                client.Dispose();
+                throw;
+            }
+
+            return client;
+        }
+    }
+}
---
+++ tests/Kros.AspNetCore.Tests/Extensions/HttpClientFactoryExtensionsShould.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Kros.AspNetCore.Net;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using System.Net.Http;
+using Xunit;
+
+namespace Kros.AspNetCore.Tests.Extensions
+{
+    public class HttpClientFactoryExtensionsShould
+    {
+        [Fact]
+        public void CopyAuthorizationHeader()
+        {
+            var httpClient = new HttpClient();
+            IHttpClientFactory httpClientFactory = Substitute.For<IHttpClientFactory>();
+            httpClientFactory.CreateClient().Returns(httpClient);
+
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Authorization"] = "Bearer sfasdfasdf414weqr";
+            IHttpContextAccessor httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            httpContextAccessor.HttpContext.Returns(httpContext);
+
+            using (HttpClient clientAuth = httpClientFactory.CreateClientWithAuthorization(httpContextAccessor))
+            {
+                clientAuth.DefaultRequestHeaders.Authorization.Should().Equals(httpContext.Request.Headers["Authorization"]);
+            }
+        }
+    }
+}
