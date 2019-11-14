207ffbfa3dc23c3de955381b32d0d2a64b1b80e2
Jakub Berthoty <berthotyms@kros.sk>
2019-11-14 11:51:05 +0100

    Multi platform targeting for Kros.AspNetCore.Tests.

--- tests/Kros.AspNetCore.Tests/Environments/EnvironmentExtensionsShould.cs
+++ tests/Kros.AspNetCore.Tests/Environments/EnvironmentExtensionsShould.cs
@@ -3,10 +3,18 @@ using Microsoft.AspNetCore.Hosting;
 using NSubstitute;
 using Xunit;
 
+#if (NETCOREAPP3_0)
+
+using Microsoft.Extensions.Hosting;
+
+#endif
+
 namespace Kros.AspNetCore.Tests.Environments
 {
     public class EnvironmentExtensionsShould
     {
+#if (NETCOREAPP2_2)
+
         [Fact]
         public void IsTestReturnTrueIfTestEnvironment()
         {
@@ -55,5 +63,58 @@ namespace Kros.AspNetCore.Tests.Environments
             env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Test);
             env.IsStagingOrProduction().Should().BeFalse();
         }
+
+#else
+
+        [Fact]
+        public void IsTestReturnTrueIfTestEnvironment()
+        {
+            IWebHostEnvironment env = Substitute.For<IWebHostEnvironment>();
+            env.EnvironmentName.Returns(EnvironmentsExtended.Test);
+
+            env.IsTest().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsTestReturnFalseIfDevelopmentEnvironment()
+        {
+            IWebHostEnvironment env = Substitute.For<IWebHostEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.Extensions.Hosting.Environments.Development);
+            env.IsTest().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsTestOrDevelopmentReturnTrueIfDevelopmentEnvironment()
+        {
+            IWebHostEnvironment env = Substitute.For<IWebHostEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.Extensions.Hosting.Environments.Development);
+            env.IsTestOrDevelopment().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsTestOrDevelopmentReturnFalseIfStagingEnvironment()
+        {
+            IWebHostEnvironment env = Substitute.For<IWebHostEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.Extensions.Hosting.Environments.Staging);
+            env.IsTestOrDevelopment().Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsStagingOrProductionReturnTrueIfStagingEnvironment()
+        {
+            IWebHostEnvironment env = Substitute.For<IWebHostEnvironment>();
+            env.EnvironmentName.Returns(Microsoft.Extensions.Hosting.Environments.Staging);
+            env.IsStagingOrProduction().Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsStagingOrProductionReturnFalseIfTestEnvironment()
+        {
+            IWebHostEnvironment env = Substitute.For<IWebHostEnvironment>();
+            env.EnvironmentName.Returns(EnvironmentsExtended.Test);
+            env.IsStagingOrProduction().Should().BeFalse();
+        }
+
+#endif
     }
 }
