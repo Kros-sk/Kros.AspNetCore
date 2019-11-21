dd0b44683c88f488de2c912075503cd0eba9b3ad
Jakub Berthoty <berthotyms@kros.sk>
2019-11-21 07:30:29 +0100

    Made Kros.AspNetCore.Tests target solely .NET Core 3.0.

--- tests/Kros.AspNetCore.Tests/Environments/EnvironmentExtensionsShould.cs
+++ tests/Kros.AspNetCore.Tests/Environments/EnvironmentExtensionsShould.cs
@@ -1,71 +1,13 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using NSubstitute;
 using Xunit;
-
-#if (NETCOREAPP3_0)
-
-using Microsoft.Extensions.Hosting;
-
-#endif
 
 namespace Kros.AspNetCore.Tests.Environments
 {
     public class EnvironmentExtensionsShould
     {
-#if (NETCOREAPP2_2)
-
-        [Fact]
-        public void IsTestReturnTrueIfTestEnvironment()
-        {
-            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
-            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Test);
-
-            env.IsTest().Should().BeTrue();
-        }
-
-        [Fact]
-        public void IsTestReturnFalseIfDevelopmentEnvironment()
-        {
-            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
-            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Development);
-            env.IsTest().Should().BeFalse();
-        }
-
-        [Fact]
-        public void IsTestOrDevelopmentReturnTrueIfDevelopmentEnvironment()
-        {
-            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
-            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Development);
-            env.IsTestOrDevelopment().Should().BeTrue();
-        }
-
-        [Fact]
-        public void IsTestOrDevelopmentReturnFalseIfStagingEnvironment()
-        {
-            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
-            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Staging);
-            env.IsTestOrDevelopment().Should().BeFalse();
-        }
-
-        [Fact]
-        public void IsStagingOrProductionReturnTrueIfStagingEnvironment()
-        {
-            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
-            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Staging);
-            env.IsStagingOrProduction().Should().BeTrue();
-        }
-
-        [Fact]
-        public void IsStagingOrProductionReturnFalseIfTestEnvironment()
-        {
-            IHostingEnvironment env = Substitute.For<IHostingEnvironment>();
-            env.EnvironmentName.Returns(Microsoft.AspNetCore.Hosting.Environments.Test);
-            env.IsStagingOrProduction().Should().BeFalse();
-        }
-
-#else
-
         [Fact]
         public void IsTestReturnTrueIfTestEnvironment()
         {
@@ -114,7 +56,5 @@ namespace Kros.AspNetCore.Tests.Environments
             env.EnvironmentName.Returns(EnvironmentsExtended.Test);
             env.IsStagingOrProduction().Should().BeFalse();
         }
-
-#endif
     }
 }
