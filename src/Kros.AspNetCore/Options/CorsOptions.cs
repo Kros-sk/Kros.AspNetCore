35346c7ed3ea0fbdc877b96606d93df57b581a46
Stano Peťko <stano@stanopetko.eu>
2019-11-30 21:23:41 +0100

    Strong typed validating configuration (#58)
    * Helpers.GetSectionName support "Settings" suffix and is case insensitive
    * Move CorsOptions to Configuration namespace
    * Add configuration validation
    * Update README
    * PR fixes

--- src/Kros.AspNetCore/Options/CorsOptions.cs
+++ src/Kros.AspNetCore/Options/CorsOptions.cs
@@ -1,18 +1,21 @@
-﻿namespace Kros.AspNetCore.Options
+﻿using System;
+
+namespace Kros.AspNetCore.Options
 {
     /// <summary>
     /// Cors options.
     /// </summary>
+    [Obsolete("Class was moved to Kros.AspNetCore.Configuration namespace.")]
     public class CorsOptions
     {
         /// <summary>
         /// Name of cors section in appsettings.json.
         /// </summary>
-        public const string CorsSectionName = "AllowedHosts";
+        public const string CorsSectionName = Kros.AspNetCore.Configuration.CorsOptions.CorsSectionName;
 
         /// <summary>
         /// Name of custom cors policy.
         /// </summary>
-        public const string CorsPolicyName = "CustomCorsPolicy";
+        public const string CorsPolicyName = Kros.AspNetCore.Configuration.CorsOptions.CorsPolicyName;
     }
 }
---
+++ tests/Kros.AspNetCore.Tests/Configuration/ConfigurationExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Kros.AspNetCore.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Kros.AspNetCore.Tests.Configuration
+{
+    public class ConfigurationExtensionsTests
+    {
+        const string TestSettingsJson = @"
+{
+  ""Test"": {
+    ""StringValue"": ""Lorem Ipsum"",
+    ""IntValue"": 42
+  }
+}";
+
+        class TestSettings
+        {
+            public string StringValue { get; set; }
+            public int IntValue { get; set; }
+        }
+
+        class ValidatableTestSettings : IValidatable
+        {
+            public string StringValue { get; set; }
+            public int IntValue { get; set; }
+
+            public void Validate()
+            {
+            }
+        }
+
+        [Fact]
+        public void ConfigureOptionsRegistersClassIntoDiContainer()
+        {
+            IConfiguration cfg = new ConfigurationBuilder()
+                .AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(TestSettingsJson)))
+                .Build();
+            var services = new ServiceCollection();
+
+            services.ConfigureOptions<TestSettings>(cfg);
+
+            ServiceProvider provider = services.BuildServiceProvider();
+            TestSettings settings = provider.GetRequiredService<TestSettings>();
+
+            settings.Should().NotBeNull();
+            settings.Should().BeEquivalentTo(new TestSettings
+            {
+                StringValue = "Lorem Ipsum",
+                IntValue = 42
+            });
+        }
+
+        [Fact]
+        public void ConfigureOptionsRegistersClassAsIValidatableIntoDiContainer()
+        {
+            IConfiguration cfg = new ConfigurationBuilder().Build();
+            var services = new ServiceCollection();
+            services.ConfigureOptions<ValidatableTestSettings>(cfg);
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            IValidatable settings = provider.GetRequiredService<IValidatable>();
+
+            settings.Should().NotBeNull();
+            settings.Should().BeOfType<ValidatableTestSettings>();
+        }
+    }
+}
