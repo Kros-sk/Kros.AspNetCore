52a7abd277b895db79a449e8bf97fed690d7a33b
M L <luhovy@kros.sk>
2020-01-22 10:23:11 +0100

    Move add json local file into extension

---
+++ src/Kros.AspNetCore/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Kros.AspNetCore.Extensions
+{
+    /// <summary>
+    /// Host builder extensions.
+    /// </summary>
+    public static class HostBuilderExtensions
+    {
+        /// <summary>
+        /// Add local.json configuration.
+        /// </summary>
+        /// <param name="hostBuilder">Host builder.</param>
+        /// <returns>The same instance of the Microsoft.Extensions.Hosting.IHostBuilder for chaining.</returns>
+        public static IHostBuilder AddLocalConfiguration(this IHostBuilder hostBuilder)
+            => hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                      .AddJsonFile("appsettings.local.json", optional: true);
+            });
+    }
+}
