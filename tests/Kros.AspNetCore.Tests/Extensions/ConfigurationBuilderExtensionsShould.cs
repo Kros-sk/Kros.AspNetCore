0ff2e96081627b5d6ad7e18dd6a9c42b9ea5871f
pistek@kros.sk <pistek@kros.sk>
2020-02-24 14:49:26 +0100

    add extension

---
+++ tests/Kros.AspNetCore.Tests/Extensions/ConfigurationBuilderExtensionsShould.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Kros.AspNetCore.Tests.Extensions
+{
+    public class ConfigurationBuilderExtensionsShould
+    {
+        [Fact]
+        public void AddConfigurationSource()
+        {
+
+        }
+    }
+}
