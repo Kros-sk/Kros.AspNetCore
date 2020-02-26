3fbb492120b61b02169889587dca555f64e78e20
pistek@kros.sk <pistek@kros.sk>
2020-02-26 10:05:10 +0100

    add unit test for AddAzureAppConfiguration extension

--- tests/Kros.AspNetCore.Tests/Extensions/ConfigurationBuilderExtensionsShould.cs
+++ tests/Kros.AspNetCore.Tests/Extensions/ConfigurationBuilderExtensionsShould.cs
@@ -1,4 +1,9 @@
 ﻿using Xunit;
+using Kros.AspNetCore.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using FluentAssertions;
 
 namespace Kros.AspNetCore.Tests.Extensions
 {
@@ -7,7 +12,25 @@ namespace Kros.AspNetCore.Tests.Extensions
         [Fact]
         public void AddConfigurationSource()
         {
+            // arrange
+            IConfigurationBuilder config = new ConfigurationBuilder();
+            HostBuilderContext builderContext = CreateHostBuilderContext();
 
+            // act
+            config.AddAzureAppConfiguration(builderContext);
+
+            // assert
+            config.Sources.Count.Should().Be(1);
         }
+
+        private HostBuilderContext CreateHostBuilderContext()
+        {
+            var context = new HostBuilderContext(new Dictionary<object, object>());
+            context.Configuration = GetConfiguration();
+            return context;
+        }
+
+        private static IConfiguration GetConfiguration()
+           => new ConfigurationBuilder().AddJsonFile("Extensions\\appsettings.configuration-test.json").Build();
     }
 }
