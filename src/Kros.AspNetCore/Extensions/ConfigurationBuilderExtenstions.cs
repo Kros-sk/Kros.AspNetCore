a91c32c4281de6c3752c419545e4393b21d4a80c
marek011 <53078235+marek011@users.noreply.github.com>
2020-02-26 12:44:44 +0100

    Azure App Configuration extension (#79)
    * add extension
    
    * add unit test for AddAzureAppConfiguration extension
    
    * add documentation

--- src/Kros.AspNetCore/Extensions/ConfigurationBuilderExtenstions.cs
+++ src/Kros.AspNetCore/Extensions/ConfigurationBuilderExtenstions.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using Kros.Extensions;
+using Azure.Identity;
 
 namespace Kros.AspNetCore.Extensions
 {
@@ -14,5 +20,44 @@ namespace Kros.AspNetCore.Extensions
         /// <returns>The same instance of the Microsoft.Extensions.Hosting.IHostBuilder for chaining.</returns>
         public static IConfigurationBuilder AddLocalConfiguration(this IConfigurationBuilder configurationBuilder)
             => configurationBuilder.AddJsonFile("appsettings.local.json", optional: true);
+
+        /// <summary>
+        /// Adds the azure application configuration.
+        /// </summary>
+        /// <param name="config">The configuration.</param>
+        /// <param name="hostingContext">The hosting context.</param>
+        /// <remarks>
+        /// Configuration should contain attributes AppConfig:Endpoint and AppConfig:Settings.
+        /// </remarks>
+        public static IConfigurationBuilder AddAzureAppConfiguration(
+            this IConfigurationBuilder config,
+            HostBuilderContext hostingContext)
+        {
+            var settings = config.Build();
+
+            config.AddAzureAppConfiguration(options =>
+            {
+                var credential = new DefaultAzureCredential();
+
+                options
+                    .Connect(new Uri(settings["AppConfig:Endpoint"]), credential)
+                    .ConfigureKeyVault(kv => kv.SetCredential(credential));
+
+                IEnumerable<string> services = settings
+                    .GetSection("AppConfig:Settings")
+                    .AsEnumerable()
+                    .Where(p => !p.Value.IsNullOrWhiteSpace())
+                    .Select(p => p.Value);
+
+                foreach (string service in services)
+                {
+                    options
+                        .Select($"{service}:*", hostingContext.HostingEnvironment.EnvironmentName)
+                        .TrimKeyPrefix($"{service}:");
+                }
+            });
+
+            return config;
+        }
     }
 }
