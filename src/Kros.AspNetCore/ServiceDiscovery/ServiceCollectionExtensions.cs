ba50beee7fb44d28338beec6336b9145e2cfa2fe
Burgyn <mino.martiniak@gmail.com>
2020-02-27 07:38:05 +0100

    Add service extension

---
+++ src/Kros.AspNetCore/ServiceDiscovery/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Kros.AspNetCore.ServiceDiscovery
+{
+    /// <summary>
+    /// Extensions for <see cref="IConfiguration"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the service discovery.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        /// <param name="options">The configure.</param>
+        public static IServiceCollection AddServiceDiscovery(
+            this IServiceCollection services,
+            Action<ServiceDiscoveryOption> options = null)
+        {
+            options?.Invoke(ServiceDiscoveryOption.Default);
+
+            services.AddScoped<IServiceDiscoveryProvider>((f) =>
+                new ServiceDiscoveryProvider(f.GetService<IConfiguration>(), ServiceDiscoveryOption.Default));
+
+            return services;
+        }
+    }
+}
---
+++ tests/Kros.AspNetCore.Tests/ServiceDiscovery/ServiceCollectionExtensionsShould.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Kros.AspNetCore.ServiceDiscovery
+{
+    public class ServiceCollectionExtensionsShould
+    {
+        [Fact]
+        public void RegisterServiceDiscoveryProvider()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+
+            serviceCollection.AddServiceDiscovery();
+
+            IServiceDiscoveryProvider provider =  serviceCollection
+                .BuildServiceProvider()
+                .GetService<IServiceDiscoveryProvider>();
+
+            provider.Should().NotBeNull();
+        }
+    }
+}
