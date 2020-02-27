5ac100eeea7a3b725eac684ea347728c20c61b29
Burgyn <mino.martiniak@gmail.com>
2020-02-27 07:10:35 +0100

    Add service discovery

---
+++ src/Kros.AspNetCore/ServiceDiscovery/ServiceDiscoveryProvider.cs
@@ -0,0 +1,41 @@
+﻿using Kros.Utils;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Kros.AspNetCore.ServiceDiscovery
+{
+    /// <summary>
+    /// Provider for obtaining service address from configuration.
+    /// </summary>
+    /// <seealso cref="Kros.AspNetCore.ServiceDiscovery.IServiceDiscoveryProvider" />
+    public class ServiceDiscoveryProvider : IServiceDiscoveryProvider
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ServiceDiscoveryOption _option;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceDiscoveryProvider"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="option">The option.</param>
+        public ServiceDiscoveryProvider(IConfiguration configuration, ServiceDiscoveryOption option)
+        {
+            _configuration = Check.NotNull(configuration, nameof(configuration));
+            _option = Check.NotNull(option, nameof(option));
+        }
+
+        /// <inheritdoc />
+        public Uri GetPath(string serviceName, string pathName)
+        {
+            var uriBuilder = new UriBuilder(GetService(serviceName));
+            uriBuilder.Path = _configuration.GetValue<string>($"{_option.SectionName}:{serviceName}:Paths:{pathName}");
+
+            return uriBuilder.Uri;
+        }
+
+
+        /// <inheritdoc />
+        public Uri GetService(string serviceName)
+            => new Uri(_configuration.GetValue<string>($"{_option.SectionName}:{serviceName}:DownstreamPath"));
+    }
+}
---
+++ tests/Kros.AspNetCore.Tests/ServiceDiscovery/ServiceDiscoveryShould.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Kros.AspNetCore.ServiceDiscovery;
+using Microsoft.Extensions.Configuration;
+using System;
+using Xunit;
+
+namespace Kros.AspNetCore.Tests.ServiceDiscovery
+{
+    public class ServiceDiscoveryShould
+    {
+        [Theory]
+        [InlineData("Users", "http://localhost:9003/")]
+        [InlineData("Projects", "http://localhost:9002/")]
+        [InlineData("Authorization", "https://authorizationservice.domain.com/")]
+        public void FindServiceUriByName(string serviceName, string expectedUri)
+        {
+            var provider = new ServiceDiscoveryProvider(GetConfiguration(), ServiceDiscoveryOption.Default);
+
+            Uri uri = provider.GetService(serviceName);
+
+            uri.AbsoluteUri.Should().Be(expectedUri);
+        }
+
+        [Theory]
+        [InlineData("Users", "getAll", "http://localhost:9003/api/users")]
+        [InlineData("Users", "getById", "http://localhost:9003/api/users/{id}")]
+        [InlineData("Projects", "create", "http://localhost:9002/api/projects")]
+        public void FindPathUriByServiceAndPathName(string serviceName, string pathName, string expectedUri)
+        {
+            var provider = new ServiceDiscoveryProvider(GetConfiguration(), ServiceDiscoveryOption.Default);
+
+            Uri uri = provider.GetPath(serviceName, pathName);
+
+            uri.ToString().Should().Be(expectedUri);
+        }
+
+        private static IConfiguration GetConfiguration() =>
+            new ConfigurationBuilder()
+                .AddJsonFile("servicediscoveryAppsettings.json")
+                .Build();
+    }
+}
