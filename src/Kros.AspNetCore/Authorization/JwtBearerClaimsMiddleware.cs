ac94ed377cfe98b001355d5c4d89f14df21f02ca
Ondrej <Lascek>
2020-11-04 11:02:19 +0100

    parsing token from header moved to function

--- src/Kros.AspNetCore/Authorization/JwtBearerClaimsMiddleware.cs
+++ src/Kros.AspNetCore/Authorization/JwtBearerClaimsMiddleware.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -35,18 +36,29 @@ namespace Kros.AspNetCore.Authorization
         /// <param name="httpContext">The http context.</param>
         public async Task Invoke(HttpContext httpContext)
         {
-            if (httpContext.Request.Headers.TryGetValue(HeaderNames.Authorization, out StringValues authHeader)
-                && authHeader.ToString().StartsWith(AuthTokenPrefix))
+            if (TryGetTokenValue(httpContext.Request.Headers, out string tokenValue)
+                && _tokenHandler.CanReadToken(tokenValue))
             {
-                string tokenValue = authHeader.ToString().Substring(AuthTokenPrefix.Length);
-                if (_tokenHandler.CanReadToken(tokenValue))
-                {
-                    JwtSecurityToken token = _tokenHandler.ReadJwtToken(tokenValue);
-                    httpContext.User.AddIdentity(new ClaimsIdentity(token.Claims));
-                }
+                JwtSecurityToken token = _tokenHandler.ReadJwtToken(tokenValue);
+                httpContext.User.AddIdentity(new ClaimsIdentity(token.Claims));
             }
 
             await _next(httpContext);
+        }
+
+        private static bool TryGetTokenValue(IHeaderDictionary headers, out string tokenValue)
+        {
+            if (headers.TryGetValue(HeaderNames.Authorization, out StringValues authHeader))
+            {
+                string authHeaderToken = authHeader.First();
+                if (authHeaderToken.StartsWith(AuthTokenPrefix))
+                {
+                    tokenValue = authHeaderToken.ToString().Substring(AuthTokenPrefix.Length);
+                    return true;
+                }
+            }
+            tokenValue = null;
+            return false;
         }
     }
 }
