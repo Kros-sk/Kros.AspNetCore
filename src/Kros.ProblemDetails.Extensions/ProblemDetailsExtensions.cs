34bb9e36b4df4c0fe23106d4b2d0dbf4a5b7ae49
Michal Aujeský <aujesky@kros.sk>
2021-01-20 20:05:47 +0100

    Added Kros ProblemDetails extensions.

---
+++ src/Kros.ProblemDetails.Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Kros.ProblemDetails.Extensions
+{
+    /// <summary>
+    /// Hellang problem details service extensions.
+    /// </summary>
+    public static class ProblemDetailsExtensions
+    {
+        /// <summary>
+        /// Registers Hellang problem details to IoC container.
+        /// It creates custom problem details for fluent <see cref="ValidationException"/>.
+        /// </summary>
+        /// <param name="services">IoC container.</param>
+        /// <param name="environment">Current environment.</param>
+        /// <param name="configAction">Additional action for configuring Hellang problem details.</param>
+        public static IServiceCollection AddCustomProblemDetails(
+            this IServiceCollection services,
+            IWebHostEnvironment environment,
+            Action<ProblemDetailsOptions> configAction = null)
+            =>  services.AddProblemDetails(p =>
+            {
+                p.IncludeExceptionDetails = (ctx, ex) => IncludeExceptionDetails(environment, ex);
+                p.Map<ValidationException>(SetProblemDetailsForFluentValidationException);
+
+                configAction?.Invoke(p);
+            });
+
+        private static bool IncludeExceptionDetails(IWebHostEnvironment environment, Exception ex)
+            => environment.IsTestOrDevelopment() && !IsExceptionWithoutExceptionDetails(ex) ? true : false;
+
+        private static bool IsExceptionWithoutExceptionDetails(Exception ex)
+            => ex.GetType() == typeof(ValidationException);
+
+        private static ValidationProblemDetails SetProblemDetailsForFluentValidationException(ValidationException ex)
+            => new ValidationProblemDetails(ex.Errors, StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Kros.ProblemDetails.Extensions/ValidationProblemDetails.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+using Hellang.Middleware.ProblemDetails;
+using Mapster;
+using System.Collections.Generic;
+
+namespace Kros.ProblemDetails.Extensions
+{
+    /// <summary>
+    /// Problem details with informations about failed fluent validations.
+    /// </summary>
+    internal class ValidationProblemDetails : StatusCodeProblemDetails
+    {
+        public IEnumerable<ValidationError> Errors { get; }
+
+        public ValidationProblemDetails(IEnumerable<ValidationFailure> errors, int statusCode) : base(statusCode)
+        {
+            Errors = errors.Adapt<IEnumerable<ValidationError>>();
+        }
+    }
+
+
+}
