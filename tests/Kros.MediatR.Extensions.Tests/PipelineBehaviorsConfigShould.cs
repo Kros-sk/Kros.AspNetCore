8795941d263b7e936336b4071eca03d2f255342d
Stano Peťko <stano@stanopetko.eu>
2021-04-19 12:57:26 +0200

    Update packages (#139)

--- tests/Kros.MediatR.Extensions.Tests/PipelineBehaviorsConfigShould.cs
+++ tests/Kros.MediatR.Extensions.Tests/PipelineBehaviorsConfigShould.cs
@@ -8,28 +8,6 @@ namespace Kros.MediatR.Extensions.Tests
     public class PipelineBehaviorsConfigShould
     {
         [Fact]
-        public void ReturnPipelineBehaviorAssembly()
-        {
-            var config = new PipelineBehaviorsConfig();
-            config.PipelineBehaviorsAssembly = typeof(string).Assembly;
-
-            IEnumerable<Assembly> assemblies = config.GetPipelineBehaviorAssemblies();
-
-            assemblies.Should().HaveCount(1);
-        }
-
-        [Fact]
-        public void ReturnRequestAssembly()
-        {
-            var config = new PipelineBehaviorsConfig();
-            config.RequestsAssembly = typeof(string).Assembly;
-
-            IEnumerable<Assembly> assemblies = config.GetRequestAssemblies();
-
-            assemblies.Should().HaveCount(1);
-        }
-
-        [Fact]
         public void IgnoreNullRequestAssembly()
         {
             var config = new PipelineBehaviorsConfig();
