8c4177dd223241d78cf146bb619c2755c0a0b713
Mruškovič Michal <mruskovicms@kros.sk>
2021-05-17 15:22:56 +0200

    ApiKeyBasicAuthenticationHandler

---
+++ src/Kros.AspNetCore/Authentication/ApiKeyBasicAuthenticationOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Kros.AspNetCore.Authentication
+{
+    /// <summary>
+    /// Options to configure the ApiKeyBasicAuthenticationHandler.
+    /// </summary>
+    class ApiKeyBasicAuthenticationOptions : AuthenticationSchemeOptions
+    {
+        /// <summary>
+        /// The API key, that will be checked during authentication.
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// Auth scheme name.
+        /// </summary>
+        public string Scheme { get; set; }
+    }
+}
