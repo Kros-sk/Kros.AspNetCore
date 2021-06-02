fd253643da3ddf7639a5f96652f2734ac569af59
Stano Peťko <stano@stanopetko.eu>
2021-06-02 13:29:53 +0200

    Remove Kros.Identity.Extensions dependency

--- src/Kros.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
+++ src/Kros.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Kros.Identity.Extensions;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
---
+++ src/Kros.AspNetCore/Extensions/IdentityServerOptions.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+namespace Kros.AspNetCore.Extensions
+{
+    /// <summary>
+    /// Class which represent settings for Identity server handler.
+    /// </summary>
+    internal class IdentityServerOptions
+    {
+        /// <summary>
+        /// Name of the API.
+        /// </summary>
+        public string ApiName { get; set; }
+
+        /// <summary>
+        /// Api secret.
+        /// </summary>
+        public string ApiSecret { get; set; }
+
+        /// <summary>
+        /// Identity server URL.
+        /// </summary>
+        public string AuthorityUrl { get; set; }
+
+        /// <summary>
+        /// Proxy configuration. If the value is <see langword="null"/> then proxy is not set.
+        /// </summary>
+        public WebProxy Proxy { get; set; }
+
+        /// <summary>
+        /// Specifies whether HTTPS is required for the discovery endpoint.
+        /// </summary>
+        public bool RequireHttpsMetadata { get; set; } = true;
+
+        /// <summary>
+        /// Authentication scheme. Required when configuring more than one handler.
+        /// </summary>
+        public string AuthenticationScheme { get; set; }
+    }
+}
