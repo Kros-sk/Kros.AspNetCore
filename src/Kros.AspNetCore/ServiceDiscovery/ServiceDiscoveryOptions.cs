1ad965acf2c5e3c2f749e267d4ed7bccbfaf5d5e
Miňo Martiniak <mino.martiniak@gmail.com>
2021-07-15 12:06:50 +0200

    Allow service as host (#148)

--- src/Kros.AspNetCore/ServiceDiscovery/ServiceDiscoveryOptions.cs
+++ src/Kros.AspNetCore/ServiceDiscovery/ServiceDiscoveryOptions.cs
@@ -11,6 +11,21 @@
         public string SectionName { get; set; } = "Services";
 
         /// <summary>
+        /// Gets or sets a value indicating whether allow service name as host if service doesn't exist in configuration.
+        /// </summary>
+        public bool AllowServiceNameAsHost { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the port. (Only if <see cref="AllowServiceNameAsHost"/> is <see langword="true"/>.)
+        /// </summary>
+        public int Port { get; set; } = 80;
+
+        /// <summary>
+        /// Gets or sets the scheme. (Only if <see cref="AllowServiceNameAsHost"/> is <see langword="true"/>.)
+        /// </summary>
+        public string Scheme { get; set; } = "http";
+
+        /// <summary>
         /// Gets the default instance.
         /// </summary>
         public static ServiceDiscoveryOptions Default { get; } = new ServiceDiscoveryOptions();
