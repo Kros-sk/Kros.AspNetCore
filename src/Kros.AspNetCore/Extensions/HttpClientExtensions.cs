e2da51af84a23d0d83b27d6499cedd4c25366934
Mrukoviè Michal <mruskovicms@kros.sk>
2021-07-27 15:52:22 +0200

    HTTP client extensions for GET request with a body

--- src/Kros.AspNetCore/Extensions/HttpClientExtensions.cs
+++ src/Kros.AspNetCore/Extensions/HttpClientExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Kros.AspNetCore.Extensions
@@ -41,6 +43,45 @@ namespace Kros.AspNetCore.Extensions
             response.ThrowIfNotSuccessStatusCode(defaultException);
             return response;
         }
+
+        /// <summary>
+        /// Get response from http request (with a body) and throws exception if request is unsuccessful.
+        /// </summary>
+        /// <param name="client">Http client.</param>
+        /// <param name="uri">Uri for request.</param>
+        /// <param name="body">Request body.</param>
+        /// <param name="defaultException">Default exception to by thrown, if failure result code is unsupported.</param>
+        public static async Task<HttpResponseMessage> GetAndCheckResponseAsync(
+            this HttpClient client,
+            Uri uri,
+            object body,
+            Exception defaultException = null)
+        {
+            string jsonBody = JsonSerializer.Serialize(body);
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = uri,
+                Content = new StringContent(jsonBody, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage response = await client.SendAsync(request);
+            response.ThrowIfNotSuccessStatusCode(defaultException);
+            return response;
+        }
+
+        /// <summary>
+        /// Get response from http request (with a body) and throws exception if request is unsuccessful.
+        /// </summary>
+        /// <param name="client">Http client.</param>
+        /// <param name="url">Url for request.</param>
+        /// <param name="body">Request body.</param>
+        /// <param name="defaultException">Default exception to by thrown, if failure result code is unsupported.</param>
+        public static Task<HttpResponseMessage> GetAndCheckResponseAsync(
+            this HttpClient client,
+            string url,
+            object body,
+            Exception defaultException = null)
+            => client.GetAndCheckResponseAsync(new Uri(url), body, defaultException);
 
         /// <summary>
         /// Get string response from http request and throws exception if request is unsuccessful.
@@ -89,6 +130,34 @@ namespace Kros.AspNetCore.Extensions
             string url,
             Exception defaultException = null)
             => await (await client.GetAndCheckResponseAsync(url, defaultException)).Content.ReadAsStreamAsync();
+
+        /// <summary>
+        /// Get stream response from http request and throws exception if request is unsuccessful.
+        /// </summary>
+        /// <param name="client">Http client.</param>
+        /// <param name="uri">Uri for request.</param>
+        /// <param name="body">Request body.</param>
+        /// <param name="defaultException">Default exception to by thrown, if failure result code is unsupported.</param>
+        public static async Task<Stream> GetStreamAndCheckResponseAsync(
+            this HttpClient client,
+            Uri uri,
+            object body,
+            Exception defaultException = null)
+            => await (await client.GetAndCheckResponseAsync(uri, body, defaultException)).Content.ReadAsStreamAsync();
+
+        /// <summary>
+        /// Get stream response from http request and throws exception if request is unsuccessful.
+        /// </summary>
+        /// <param name="client">Http client.</param>
+        /// <param name="url">Url for request.</param>
+        /// <param name="body">Request body.</param>
+        /// <param name="defaultException">Default exception to by thrown, if failure result code is unsupported.</param>
+        public static async Task<Stream> GetStreamAndCheckResponseAsync(
+            this HttpClient client,
+            string url,
+            object body,
+            Exception defaultException = null)
+            => await (await client.GetAndCheckResponseAsync(url, body, defaultException)).Content.ReadAsStreamAsync();
 
         /// <summary>
         /// Get byte array response from http request and throws exception if request is unsuccessful.
