913cdfc67e165b5b7086ebef7a83ba7e1a790468
pistek <pistek@kros.sk>
2021-08-11 15:39:27 +0200

    create InvalidEntityReferencesException and InvalidEntityReferencesProblemDetails

---
+++ src/Kros.AspNetCore/Exceptions/InvalidEntityReferencesException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kros.AspNetCore.Exceptions
+{
+    /// <summary>
+    /// Exception is thrown when entity references in user request are invalid.
+    /// </summary>
+    public class InvalidEntityReferencesException: Exception
+    {
+        /// <summary>
+        /// Ids of invalid entities. Dictionary with key = entity name and value = enumerable of invalid entity ids.
+        /// </summary>
+        public IDictionary<string, IEnumerable<long>> Ids { get; }
+
+        /// <summary>
+        /// Type of entity references problem. The value should follow the rules for problem
+        /// types defined in https://datatracker.ietf.org/doc/html/rfc7807.
+        /// </summary>
+        public string Type { get; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        /// <param name="type">Type of entity references problem. <see cref="Type"/></param>
+        /// <param name="ids">Ids of invalid entities. <see cref="Ids"/></param>
+        /// <param name="message">Message with detail of exception.</param>
+        public InvalidEntityReferencesException(string type, IDictionary<string, IEnumerable<long>> ids, string message = null)
+            : base(message)
+        {
+            Type = type;
+            Ids = ids;
+        }
+    }
+}
---
+++ src/Kros.ProblemDetails.Extensions/InvalidEntityReferencesProblemDetails.cs
@@ -0,0 +1,21 @@
+﻿using Hellang.Middleware.ProblemDetails;
+using System.Collections.Generic;
+
+namespace Kros.ProblemDetails.Extensions
+{
+    /// <summary>
+    /// Problem details with information about invalid entity references.
+    /// </summary>
+    internal class InvalidEntityReferencesProblemDetails : StatusCodeProblemDetails
+    {
+        /// <summary>
+        /// Ids of invalid entities. Dictionary with key = entity name and value = enumerable of invalid entity ids.
+        /// </summary>
+        public IDictionary<string, IEnumerable<long>> Ids { get; set; }
+
+        public InvalidEntityReferencesProblemDetails(IDictionary<string, IEnumerable<long>> ids, int statusCode) : base(statusCode)
+        {
+            Ids = ids;
+        }
+    }
+}
