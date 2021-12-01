19b9799b83291a69e22113a9d17dc1fbc659d716
Mruškovič Michal <mruskovic@kros.sk>
2021-12-01 10:33:04 +0100

    Change mapping comparer to culture invariant.

--- src/Kros.AspNetCore/JsonPatch/JsonPatchMapperConfig.cs
+++ src/Kros.AspNetCore/JsonPatch/JsonPatchMapperConfig.cs
@@ -14,7 +14,7 @@ namespace Kros.AspNetCore.JsonPatch
     {
         private Func<string, string> _pathMapping;
         private ConcurrentDictionary<string, string> _mapping =
-            new ConcurrentDictionary<string, string>(StringComparer.CurrentCulture);
+            new ConcurrentDictionary<string, string>(StringComparer.InvariantCulture);
 
         /// <summary>
         /// Creates new config for <typeparamref name="TSource"/> and store it for non parametric  extension
