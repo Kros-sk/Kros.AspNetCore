5026dc665b313e6bbb0adbe474362f418441cbea
Stano Peťko <stano@stanopetko.eu>
2022-10-03 14:50:59 +0200

    Implement StringEnumFIlter

--- src/Kros.Swagger.Extensions/SwaggerGenOptionsExtensions.cs
+++ src/Kros.Swagger.Extensions/SwaggerGenOptionsExtensions.cs
@@ -13,28 +13,144 @@ namespace Kros.Swagger.Extensions
     public static class SwaggerGenOptionsExtensions
     {
         /// <summary>
-        /// Add <see cref="EnumSchemaFilter"/> to Swagger generator options.
+        /// Document all enum types with string enum values.
         /// </summary>
         /// <param name="opt">Swagger generator options.</param>
-        /// <param name="enumTypes">Enum types to extend. All types must be of Enum type, or <see cref="ArgumentException"/>
-        /// will be thrown.</param>
-        /// <param name="schemaAction">Additional schema action for manual tweaking.</param>
+        /// <param name="xmlDocPaths">
+        /// <inheritdoc
+        ///     cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='xmlDocPaths']"/>
+        /// </param>
+        /// <param name="schemaAction">
+        /// <inheritdoc
+        ///     cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='schemaAction']"/>
+        /// </param>
+        /// <param name="parameterAction">
+        /// <inheritdoc
+        ///     cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='parameterAction']"/>
+        /// </param>
         /// <returns>Input <paramref name="opt"/> value for fluent chaining.</returns>
-        public static SwaggerGenOptions AddStringEnumSchemaFilter(
+        public static SwaggerGenOptions DocumentAllEnumsWithStrings(
+            this SwaggerGenOptions opt,
+            IEnumerable<string> xmlDocPaths = null,
+            Action<OpenApiSchema, SchemaFilterContext> schemaAction = null,
+            Action<OpenApiParameter, ParameterFilterContext> parameterAction = null)
+        {
+            opt.SchemaFilter<StringEnumFilter>(
+                xmlDocPaths ?? StringEnumFilter.DummyXmlDocPaths,
+                schemaAction ?? StringEnumFilter.DummySchemaAction,
+                parameterAction ?? StringEnumFilter.DummyParameterAction);
+
+            opt.ParameterFilter<StringEnumFilter>(
+                xmlDocPaths ?? StringEnumFilter.DummyXmlDocPaths,
+                schemaAction ?? StringEnumFilter.DummySchemaAction,
+                parameterAction ?? StringEnumFilter.DummyParameterAction);
+
+            return opt;
+        }
+
+        /// <summary>
+        /// Document enum types filtered by <paramref name="enumTypeFilter"/> with string values.
+        /// </summary>
+        /// <param name="opt">
+        ///     <inheritdoc
+        ///         cref="DocumentAllEnumsWithStrings(SwaggerGenOptions, IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///         path="//param[@name='opt']"/>
+        /// </param>
+        /// <param name="enumTypeFilter">Function, which filters which types will be documented with string values.
+        /// The types still must be enum types.</param>
+        /// <param name="xmlDocPaths">
+        ///     <inheritdoc
+        ///         cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///         path="//param[@name='xmlDocPaths']"/>
+        /// </param>
+        /// <param name="schemaAction">
+        ///     <inheritdoc
+        ///         cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///         path="//param[@name='schemaAction']"/>
+        /// </param>
+        /// <param name="parameterAction">
+        ///     <inheritdoc
+        ///         cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///         path="//param[@name='parameterAction']"/>
+        /// </param>
+        /// <returns>
+        ///     <inheritdoc
+        ///         cref="DocumentAllEnumsWithStrings(SwaggerGenOptions, IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///         path="//returns"/>
+        /// </returns>
+        public static SwaggerGenOptions DocumentEnumsWithStrings(
+            this SwaggerGenOptions opt,
+            Func<Type, bool> enumTypeFilter,
+            IEnumerable<string> xmlDocPaths = null,
+            Action<OpenApiSchema, SchemaFilterContext> schemaAction = null,
+            Action<OpenApiParameter, ParameterFilterContext> parameterAction = null)
+        {
+            opt.SchemaFilter<StringEnumFilter>(
+                enumTypeFilter,
+                xmlDocPaths ?? StringEnumFilter.DummyXmlDocPaths,
+                schemaAction ?? StringEnumFilter.DummySchemaAction,
+                parameterAction ?? StringEnumFilter.DummyParameterAction);
+
+            opt.ParameterFilter<StringEnumFilter>(
+                enumTypeFilter,
+                xmlDocPaths ?? StringEnumFilter.DummyXmlDocPaths,
+                schemaAction ?? StringEnumFilter.DummySchemaAction,
+                parameterAction ?? StringEnumFilter.DummyParameterAction);
+
+            return opt;
+        }
+
+        /// <summary>
+        /// Document specified enum types (<paramref name="enumTypes"/>) with string values.
+        /// </summary>
+        /// <param name="opt">
+        /// <inheritdoc
+        ///     cref="DocumentAllEnumsWithStrings(SwaggerGenOptions, IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='opt']"/>
+        /// </param>
+        /// <param name="enumTypes">List of enum types, which will be documented with string values.</param>
+        /// <param name="xmlDocPaths">
+        /// <inheritdoc
+        ///     cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='xmlDocPaths']"/>
+        /// </param>
+        /// <param name="schemaAction">
+        /// <inheritdoc
+        ///     cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='schemaAction']"/>
+        /// </param>
+        /// <param name="parameterAction">
+        /// <inheritdoc
+        ///     cref="StringEnumFilter(IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///     path="//param[@name='parameterAction']"/>
+        /// </param>
+        /// <returns>
+        ///     <inheritdoc
+        ///         cref="DocumentAllEnumsWithStrings(SwaggerGenOptions, IEnumerable{string}, Action{OpenApiSchema, SchemaFilterContext}, Action{OpenApiParameter, ParameterFilterContext})"
+        ///         path="//returns"/>
+        /// </returns>
+        public static SwaggerGenOptions DocumentEnumsWithStrings(
             this SwaggerGenOptions opt,
             IEnumerable<Type> enumTypes,
             IEnumerable<string> xmlDocPaths = null,
             Action<OpenApiSchema, SchemaFilterContext> schemaAction = null,
             Action<OpenApiParameter, ParameterFilterContext> parameterAction = null)
         {
-            opt.SchemaFilter<EnumSchemaFilter>(
+            opt.SchemaFilter<StringEnumFilter>(
                 enumTypes,
-                xmlDocPaths ?? EnumHelpers.DummyXmlDocPaths,
-                schemaAction ?? EnumHelpers.DummySchemaAction);
-            opt.ParameterFilter<EnumParameterFilter>(
+                xmlDocPaths ?? StringEnumFilter.DummyXmlDocPaths,
+                schemaAction ?? StringEnumFilter.DummySchemaAction,
+                parameterAction ?? StringEnumFilter.DummyParameterAction);
+
+            opt.ParameterFilter<StringEnumFilter>(
                 enumTypes,
-                xmlDocPaths ?? EnumHelpers.DummyXmlDocPaths,
-                parameterAction ?? EnumHelpers.DummyParameterAction);
+                xmlDocPaths ?? StringEnumFilter.DummyXmlDocPaths,
+                schemaAction ?? StringEnumFilter.DummySchemaAction,
+                parameterAction ?? StringEnumFilter.DummyParameterAction);
+
             return opt;
         }
 
@@ -46,21 +162,33 @@ namespace Kros.Swagger.Extensions
         /// <param name="filePathFilter">Function to test if file will be included. If not set, all <c>.xml</c> files in
         /// <paramref name="folderPath"/> folder will be included.</param>
         /// <returns>Input <paramref name="opt"/> value for fluent chaining.</returns>
-        public static SwaggerGenOptions IncludeXmlCommentsFromAllXmlFiles(
+        public static List<string> IncludeXmlCommentsFromAllFiles(
             this SwaggerGenOptions opt,
             string folderPath,
             Func<string, bool> filePathFilter = null)
         {
+            List<string> usedXmlFiles = new List<string>();
             foreach (string filePath in Directory.EnumerateFiles(folderPath, "*.xml"))
             {
                 bool addFile = filePathFilter is null || filePathFilter(filePath);
                 if (addFile)
                 {
                     opt.IncludeXmlComments(filePath);
+                    usedXmlFiles.Add(filePath);
                 }
             }
-            return opt;
+            return usedXmlFiles;
         }
+
+        /// <summary>
+        /// Include all XML comment files from current domain base folder: <c>AppDomain.CurrentDomain.BaseDirectory</c>
+        /// (<see cref="AppDomain.BaseDirectory"/>).
+        /// </summary>
+        /// <param name="opt">Swagger generator options.</param>
+        /// <returns>Input <paramref name="opt"/> value for fluent chaining.</returns>
+        public static List<string> IncludeXmlCommentsFromAllFilesInCurrentDomainBaseDirectory(
+            this SwaggerGenOptions opt)
+            => IncludeXmlCommentsFromAllFiles(opt, AppDomain.CurrentDomain.BaseDirectory);
 
         internal static SwaggerGenOptions IncludeXmlCommentsFromCurrentDomainAssembly(this SwaggerGenOptions opt)
         {
