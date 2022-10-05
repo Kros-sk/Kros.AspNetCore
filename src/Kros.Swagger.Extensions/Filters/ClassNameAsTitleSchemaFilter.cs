eaf673c78a8df97f605d0b35f6ef9916caa9d9de
Stano Peťko <stano@stanopetko.eu>
2022-10-05 12:25:13 +0200

    Swagger: use nullable flag and class name as title (#169)

---
+++ src/Kros.Swagger.Extensions/Filters/ClassNameAsTitleSchemaFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+
+namespace Kros.Swagger.Extensions.Filters
+{
+    internal sealed class ClassNameAsTitleSchemaFilter : ISchemaFilter
+    {
+        void ISchemaFilter.Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            if (string.IsNullOrEmpty(schema.Title)
+                && context.Type != null
+                && !IsPrimitive(context.Type))
+            {
+                schema.Title = context.Type.Name;
+            }
+        }
+
+        private static bool IsPrimitive(Type type)
+        {
+            // String is a primitive type for these purposes.
+            if (type.IsPrimitive || type == typeof(string))
+            {
+                return true;
+            }
+            Type nullableType = Nullable.GetUnderlyingType(type);
+            if (nullableType != null)
+            {
+                return nullableType.IsPrimitive || nullableType == typeof(string);
+            }
+            return false;
+        }
+    }
+}
