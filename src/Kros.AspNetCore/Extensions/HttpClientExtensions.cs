b1dd91100cfecd73ada31d1372659346d302bef7
Matej Martiniak <matej.martiniak@kros.sk>
2022-11-07 11:30:55 +0100

    Added cancellation token for httpClientExtensions

--- src/Kros.AspNetCore/Extensions/HttpClientExtensions.cs
+++ src/Kros.AspNetCore/Extensions/HttpClientExtensions.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Kros.AspNetCore.Extensions
@@ -18,12 +19,14 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="uri">Uri for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<HttpResponseMessage> GetAndCheckResponseAsync(
             this HttpClient client,
             Uri uri,
-            Exception defaultException = null)
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
         {
-            HttpResponseMessage response = await client.GetAsync(uri);
+            HttpResponseMessage response = await client.GetAsync(uri, cancellationToken);
             await response.ThrowIfNotSuccessStatusCodeAndKeepPayload(defaultException);
 
             return response;
@@ -35,12 +38,14 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="url">Url for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<HttpResponseMessage> GetAndCheckResponseAsync(
             this HttpClient client,
             string url,
-            Exception defaultException = null)
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
         {
-            HttpResponseMessage response = await client.GetAsync(url);
+            HttpResponseMessage response = await client.GetAsync(url, cancellationToken);
             await response.ThrowIfNotSuccessStatusCodeAndKeepPayload(defaultException);
 
             return response;
@@ -53,11 +58,13 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="uri">Uri for request.</param>
         /// <param name="body">Request body.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<HttpResponseMessage> GetAndCheckResponseAsync(
             this HttpClient client,
             Uri uri,
             object body,
-            Exception defaultException = null)
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
         {
             string jsonBody = JsonSerializer.Serialize(body);
             var request = new HttpRequestMessage
@@ -66,7 +73,7 @@ namespace Kros.AspNetCore.Extensions
                 RequestUri = uri,
                 Content = new StringContent(jsonBody, Encoding.UTF8, "application/json")
             };
-            HttpResponseMessage response = await client.SendAsync(request);
+            HttpResponseMessage response = await client.SendAsync(request, cancellationToken);
             await response.ThrowIfNotSuccessStatusCodeAndKeepPayload(defaultException);
 
             return response;
@@ -79,12 +86,14 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="url">Url for request.</param>
         /// <param name="body">Request body.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static Task<HttpResponseMessage> GetAndCheckResponseAsync(
             this HttpClient client,
             string url,
             object body,
-            Exception defaultException = null)
-            => client.GetAndCheckResponseAsync(new Uri(url), body, defaultException);
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => client.GetAndCheckResponseAsync(new Uri(url), body, defaultException, cancellationToken);
 
         /// <summary>
         /// Get string response from http request and throws exception if request is unsuccessful.
@@ -92,11 +101,13 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="uri">Uri for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<string> GetStringAndCheckResponseAsync(
             this HttpClient client,
             Uri uri,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(uri, defaultException)).Content.ReadAsStringAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(uri, defaultException, cancellationToken)).Content.ReadAsStringAsync();
 
         /// <summary>
         /// Get string response from http request and throws exception if request is unsuccessful.
@@ -104,11 +115,13 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="url">Url for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<string> GetStringAndCheckResponseAsync(
             this HttpClient client,
             string url,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(url, defaultException)).Content.ReadAsStringAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(url, defaultException, cancellationToken)).Content.ReadAsStringAsync();
 
         /// <summary>
         /// Get stream response from http request and throws exception if request is unsuccessful.
@@ -116,11 +129,13 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="uri">Uri for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<Stream> GetStreamAndCheckResponseAsync(
             this HttpClient client,
             Uri uri,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(uri, defaultException)).Content.ReadAsStreamAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(uri, defaultException, cancellationToken)).Content.ReadAsStreamAsync();
 
         /// <summary>
         /// Get stream response from http request and throws exception if request is unsuccessful.
@@ -128,11 +143,13 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="url">Url for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<Stream> GetStreamAndCheckResponseAsync(
             this HttpClient client,
             string url,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(url, defaultException)).Content.ReadAsStreamAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(url, defaultException, cancellationToken)).Content.ReadAsStreamAsync();
 
         /// <summary>
         /// Get stream response from http request and throws exception if request is unsuccessful.
@@ -141,12 +158,14 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="uri">Uri for request.</param>
         /// <param name="body">Request body.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<Stream> GetStreamAndCheckResponseAsync(
             this HttpClient client,
             Uri uri,
             object body,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(uri, body, defaultException)).Content.ReadAsStreamAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(uri, body, defaultException, cancellationToken)).Content.ReadAsStreamAsync();
 
         /// <summary>
         /// Get stream response from http request and throws exception if request is unsuccessful.
@@ -155,12 +174,14 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="url">Url for request.</param>
         /// <param name="body">Request body.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<Stream> GetStreamAndCheckResponseAsync(
             this HttpClient client,
             string url,
             object body,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(url, body, defaultException)).Content.ReadAsStreamAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(url, body, defaultException, cancellationToken)).Content.ReadAsStreamAsync();
 
         /// <summary>
         /// Get byte array response from http request and throws exception if request is unsuccessful.
@@ -168,11 +189,13 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="uri">Uri for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<byte[]> GetByteArrayAndCheckResponseAsync(
             this HttpClient client,
             Uri uri,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(uri, defaultException)).Content.ReadAsByteArrayAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(uri, defaultException, cancellationToken)).Content.ReadAsByteArrayAsync();
 
         /// <summary>
         /// Get byte array response from http request and throws exception if request is unsuccessful.
@@ -180,10 +203,12 @@ namespace Kros.AspNetCore.Extensions
         /// <param name="client">Http client.</param>
         /// <param name="url">Url for request.</param>
         /// <param name="defaultException">Default exception to be thrown, if failure result code is unsupported.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public static async Task<byte[]> GetByteArrayAndCheckResponseAsync(
             this HttpClient client,
             string url,
-            Exception defaultException = null)
-            => await (await client.GetAndCheckResponseAsync(url, defaultException)).Content.ReadAsByteArrayAsync();
+            Exception defaultException = null,
+            CancellationToken cancellationToken = default)
+            => await (await client.GetAndCheckResponseAsync(url, defaultException, cancellationToken)).Content.ReadAsByteArrayAsync();
     }
 }
