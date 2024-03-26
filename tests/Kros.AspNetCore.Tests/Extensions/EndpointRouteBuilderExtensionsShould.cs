1611f94f0bc6606718c1f96acfca8b967736668f
matej.martiniak <martiniak.matej@gmail.com>
2024-03-26 11:25:30 +0100

    Added extension method MapSignalRHubWithOptions to utilize setting its properties through configuration
    We would change some of signalR default properties, for example max size of message. With this method, it can be set in app configuration instead changing it in code directly.
    
    HttpConnectionDispatcherOptions has default values, if we change some in configuration, it accepts it. In hub configuration we set this as option object, instead of mapping properties.

---
+++ tests/Kros.AspNetCore.Tests/Extensions/EndpointRouteBuilderExtensionsShould.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Kros.AspNetCore.Extensions;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.SignalR;
+using NSubstitute;
+using Xunit;
+
+namespace Kros.AspNetCore.Tests.Extensions
+{
+    public class EndpointRouteBuilderExtensionsShould
+    {
+        [Fact]
+        public void ThrowExceptionIfHttpContextDispatcherOptionsAreMissing()
+        {
+            IConfiguration configuration = GetBadConfiguration();
+            IEndpointRouteBuilder endpoints = Substitute.For<IEndpointRouteBuilder>();
+
+            Action action = () => endpoints.MapSignalRHubWithOptions<Hub>(configuration, "Route");
+
+            action.Should().Throw<InvalidOperationException>(Arg.Any<string>());
+        }
+
+        [Fact]
+        public void NotThrowExceptionIfHttpContextDispatcherOptionsAreSet()
+        {
+            IConfiguration configuration = GetConfiguration();
+            IEndpointRouteBuilder endpoints = Substitute.For<IEndpointRouteBuilder>();
+
+            Action action = () => endpoints.MapSignalRHubWithOptions<Hub>(configuration, "Route");
+
+            action.Should().NotThrow<InvalidOperationException>(Arg.Any<string>());
+        }
+
+        [Fact]
+        public void HaveOptionsCorrectlySet()
+        {
+            IConfiguration configuration = GetConfiguration();
+            HttpConnectionDispatcherOptions options = new HttpConnectionDispatcherOptions();
+
+            HttpConnectionDispatcherOptions fromCfg = configuration.GetSection<HttpConnectionDispatcherOptions>();
+
+            options.ApplicationMaxBufferSize.Should().Be(fromCfg.ApplicationMaxBufferSize);
+            options.TransportMaxBufferSize.Should().NotBe(fromCfg.TransportMaxBufferSize);
+        }
+
+        private static IConfiguration GetBadConfiguration()
+           => new ConfigurationBuilder().AddJsonFile(Path.Combine("Extensions", "appsettings.configuration-test-signalr-bad.json")).Build();
+
+        private static IConfiguration GetConfiguration()
+            => new ConfigurationBuilder().AddJsonFile(Path.Combine("Extensions", "appsettings.configuration-test.json")).Build();
+    }
+}
