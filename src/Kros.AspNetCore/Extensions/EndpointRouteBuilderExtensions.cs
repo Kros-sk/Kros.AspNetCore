558440a6a2185c033ad99d9029177c65e71f15d9
bakosk <37989549+bakosk@users.noreply.github.com>
2024-03-26 13:38:20 +0100

    Added extension method MapSignalRHubWithOptions to utilize setting it… (#195)
    * Added extension method MapSignalRHubWithOptions to utilize setting its properties through configuration
    
    We would change some of signalR default properties, for example max size of message. With this method, it can be set in app configuration instead changing it in code directly.
    
    HttpConnectionDispatcherOptions has default values, if we change some in configuration, it accepts it. In hub configuration we set this as option object, instead of mapping properties.
    
    * update version

---
+++ src/Kros.AspNetCore/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Kros.AspNetCore.Extensions
+{
+    /// <summary>
+    /// Extensions for <see cref="IEndpointRouteBuilder"/>.
+    /// </summary>
+    public static class EndpointRouteBuilderExtensions
+    {
+        /// <summary>
+        /// Maps signalr hub with options from configuration.
+        /// </summary>
+        /// <param name="endpoints"><see cref="IEndpointRouteBuilder"/> where middleware is added.</param>
+        /// <param name="configuration">Configuration from which the options are loaded.
+        /// Configuration must contains ClientCredentialsAuthorization section.</param>
+        /// <param name="pattern">Signalr hub endpoint route pattern.</param>
+        /// <exception cref="InvalidOperationException">
+        /// When `HttpConnectionDispatcher` section is missing in configuration.
+        /// </exception>
+        public static void MapSignalRHubWithOptions<THub>(
+            this IEndpointRouteBuilder endpoints,
+            IConfiguration configuration,
+            [StringSyntax("Route")] string pattern)  where THub : Hub
+        {
+            HttpConnectionDispatcherOptions options = configuration.GetSection<HttpConnectionDispatcherOptions>();
+
+            if (options is null)
+            {
+                throw new InvalidOperationException(
+                    string.Format(Properties.Resources.OptionMissingSection,
+                        Helpers.GetSectionName<HttpConnectionDispatcherOptions>()));
+            }
+
+            endpoints.MapHub<THub>(pattern, o =>
+            {
+                o = options;
+            });
+        }
+    }
+}
