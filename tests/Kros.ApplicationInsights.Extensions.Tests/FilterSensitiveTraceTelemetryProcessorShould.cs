1cb736aac361bdf47f0ef2efab7b11facaa39f10
Spullmann <pullmann@kros.sk>
2025-05-16 06:42:37 +0200

    Add telemetry filter processor for sensitive data (#205)

---
+++ tests/Kros.ApplicationInsights.Extensions.Tests/FilterSensitiveTraceTelemetryProcessorShould.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Xunit;
+
+namespace Kros.ApplicationInsights.Extensions.Tests
+{
+    public class FilterSensitiveTraceTelemetryProcessorShould
+    {
+        private class ReceivingTestProcessor : ITelemetryProcessor
+        {
+            public ITelemetry ReceivedTelemetry { get; private set; }
+
+            public void Process(ITelemetry item)
+            {
+                ReceivedTelemetry = item;
+            }
+        }
+
+        [Fact]
+        public void PassTelemetry_WhenMessageDoesNotContainSensitiveData()
+        {
+            var next = new ReceivingTestProcessor();
+            var processor = new FilterSensitiveTraceTelemetryProcessor(next);
+            var trace = new TraceTelemetry("This is a harmless message.");
+
+            processor.Process(trace);
+
+            next.ReceivedTelemetry.Should().Be(trace);
+        }
+
+        [Theory]
+        [InlineData("Request Headers:\r\nAuthorization: SecretKey")]
+        [InlineData("Request Headers:\r\nx-functions-key: SecretKey")]
+        public void FilterOutTelemetry_WhenMessageContainsSensitiveData(string sensitiveMessage)
+        {
+            var next = new ReceivingTestProcessor();
+            var processor = new FilterSensitiveTraceTelemetryProcessor(next);
+            var trace = new TraceTelemetry(sensitiveMessage);
+
+            processor.Process(trace);
+
+            next.ReceivedTelemetry.Should().BeNull();
+        }
+
+        [Fact]
+        public void PassNonTraceTelemetry()
+        {
+            var next = new ReceivingTestProcessor();
+            var processor = new FilterSensitiveTraceTelemetryProcessor(next);
+            var eventTelemetry = new EventTelemetry("TestEvent");
+
+            processor.Process(eventTelemetry);
+
+            next.ReceivedTelemetry.Should().Be(eventTelemetry);
+        }
+    }
+}
